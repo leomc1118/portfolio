1ca42927436074e24bca3a930cbe57be9d925786
leomc1118 <classyleo1800@gmail.com>
2024-08-02 21:51:57 +0900

    updated URL for threejs glb

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,10 +12,7 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB =
-    (process.env.NODE_ENV === 'production'
-      ? 'https://craftzdog.global.ssl.fastly.net/homepage'
-      : '') + '/Agera.glb'
+  const urlDogGLB = '/Agera.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
