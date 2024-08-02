155306c3612c74d112d4411f9c7b3746bbf55959
leomc1118 <classyleo1800@gmail.com>
2024-08-02 21:45:21 +0900

    fixing unused vars

--- pages/works/mbta.js
+++ pages/works/mbta.js
@@ -1,17 +1,6 @@
-import {
-  Container,
-  Badge,
-  Link,
-  List,
-  ListItem,
-  UnorderedList,
-  Heading,
-  Center,
-  Image
-} from '@chakra-ui/react'
+import { Container, Badge } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, Meta } from '../../components/work'
+import { Title } from '../../components/work'
 import P from '../../components/paragraph'
 
 const Projects = () => (
