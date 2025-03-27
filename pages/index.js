6e239c8519a870d885522f295180cbbba39f25ee
leomc1118 <classyleo1800@gmail.com>
2025-03-27 19:13:40 -0400

    Added Breadcrumbs

--- pages/index.js
+++ pages/index.js
@@ -168,6 +168,66 @@ const Home = () => (
       </Section>
       <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
+          Breadcrumbs..
+        </Heading>
+        <Heading as="h4" variant="section-title">
+          I. Infinite Fireability
+        </Heading>
+        <Paragraph>
+          The scale of fireability. When thinking about yourself as an employee,
+          place yourself on the following scale. At one end, you have the 10x
+          employee who keeps everything in their head and, if they left, you
+          would not be able to replace them at all. This person is infinitely
+          unfireable. At the other end, you have the employee that writes
+          everything down and makes their work reproduceable and easy to onboard
+          another person to do the same thing. This person is infinitely
+          fireable. Theoretically, the infinitely unfireable engineer is more
+          likely to survive a round of layoffs. But when they&apos;re at the
+          right company, the company will keep the infinitely fireable employee.
+          Coworkers also prefer the infinitely fireable employee, and coworkers
+          tend to be the connections that matter more during a time where
+          connections have an increasing impact on where you end up.
+        </Paragraph>
+        <Heading as="h4" variant="section-title">
+          II. The Main Thing is to keep the Main Thing the Main Thing
+        </Heading>
+        <Paragraph>
+          The "Main Thing" refers to what made your business/team great in the
+          first place. Scope creep and features that don&apos;t serve to make
+          the Main Thing better should be politely turned away. A Hunanese
+          restaurant should focus on increasing the appeal of Hunanese cuisine
+          rather than investing in adding boba to their menu.
+        </Paragraph>
+        <Heading as="h4" variant="section-title">
+          III. Waste
+        </Heading>
+        <Paragraph>
+          It&apos;s easy to track physical waste. However, from the lens of
+          knowledge work, knowledge waste has the potential to be just as costly
+          to a business. Minimizing knowledge waste could take the form of
+          developing personnel management systems that reward individuals and
+          teams for creating and teaching knowledge that contributes to robust
+          profitability. For example, having a rotating host of a talk for a
+          member of the team to teach other members an interesting problem they
+          solved recently or going over how a subsystem was designed. Another
+          example could be having formal retrospectives on a project.
+        </Paragraph>
+        <Heading as="h4" variant="section-title">
+          IV. On Being Bored
+        </Heading>
+        <Paragraph>
+          Find some time every week to give yourself to be bored. We&apos;re
+          evolved to let our minds wander regularly and failure to give yourself
+          time to do so means you will go crazy!!Too stimulating. Time to be
+          bored also gives time to reflect and think of new ideas. This also
+          feeds into the idea of hanging out with yourself. Going out and
+          spending time WITH yourself, not by yourself, can be revealing. You
+          will get to a point where you can make your own fun and humor
+          yourself. :)
+        </Paragraph>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
           My Links
         </Heading>
         <List>
